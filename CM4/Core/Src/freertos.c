d94b74ea2062c6016038e295d52376d4b3c6bfa9
DucTai189 <tainguyen189@gmail.com>
2026-04-20 22:50:09 +1000

    Initial commit: FreeRTOS Foundation exercise 1.1

---
+++ CM4/Core/Src/freertos.c
@@ -0,0 +1,225 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * File Name          : freertos.c
+  * Description        : Code for freertos applications
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "FreeRTOS.h"
+#include "task.h"
+#include "main.h"
+#include "FreeRTOS.h"
+#include "cmsis_os2.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "queue.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+typedef StaticTask_t osStaticThreadDef_t;
+typedef StaticQueue_t osStaticMessageQDef_t;
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+typedef struct
+{
+    uint8_t led_state_u8;
+    uint8_t Led_selected_u8;
+
+}Led_Control_st;
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN Variables */
+Led_Control_st Led_Control_t ={
+  .led_state_u8 = 0,
+  .Led_selected_u8 = 0,
+};
+/* USER CODE END Variables */
+/* Definitions for defaultTask */
+osThreadId_t defaultTaskHandle;
+const osThreadAttr_t defaultTask_attributes = {
+  .name = "defaultTask",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityNormal,
+};
+/* Definitions for Tsk_ControlLed */
+osThreadId_t Tsk_ControlLedHandle;
+uint32_t Tsk_ControlLedBuffer[ 128 ];
+osStaticThreadDef_t Tsk_ControlLedControlBlock;
+const osThreadAttr_t Tsk_ControlLed_attributes = {
+  .name = "Tsk_ControlLed",
+  .cb_mem = &Tsk_ControlLedControlBlock,
+  .cb_size = sizeof(Tsk_ControlLedControlBlock),
+  .stack_mem = &Tsk_ControlLedBuffer[0],
+  .stack_size = sizeof(Tsk_ControlLedBuffer),
+  .priority = (osPriority_t) osPriorityLow,
+};
+/* Definitions for Trigger_LED */
+osMessageQueueId_t Trigger_LEDHandle;
+uint8_t Trigger_LEDBuffer[ 16 * sizeof( uint16_t ) ];
+osStaticMessageQDef_t Trigger_LEDControlBlock;
+const osMessageQueueAttr_t Trigger_LED_attributes = {
+  .name = "Trigger_LED",
+  .cb_mem = &Trigger_LEDControlBlock,
+  .cb_size = sizeof(Trigger_LEDControlBlock),
+  .mq_mem = &Trigger_LEDBuffer,
+  .mq_size = sizeof(Trigger_LEDBuffer)
+};
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN FunctionPrototypes */
+
+/* USER CODE END FunctionPrototypes */
+
+void StartDefaultTask(void *argument);
+void StartTask03(void *argument);
+
+void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
+
+/**
+  * @brief  FreeRTOS initialization
+  * @param  None
+  * @retval None
+  */
+void MX_FREERTOS_Init(void) {
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+  /* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+  /* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* Create the queue(s) */
+  /* creation of Trigger_LED */
+  Trigger_LEDHandle = osMessageQueueNew (16, sizeof(uint16_t), &Trigger_LED_attributes);
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+  /* add queues, ... */
+  /* USER CODE END RTOS_QUEUES */
+
+  /* Create the thread(s) */
+  /* creation of defaultTask */
+  defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
+
+  /* creation of Tsk_ControlLed */
+  Tsk_ControlLedHandle = osThreadNew(StartTask03, NULL, &Tsk_ControlLed_attributes);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+  /* add threads, ... */
+  /* USER CODE END RTOS_THREADS */
+
+  /* USER CODE BEGIN RTOS_EVENTS */
+  /* add events, ... */
+  /* USER CODE END RTOS_EVENTS */
+
+}
+
+/* USER CODE BEGIN Header_StartDefaultTask */
+/**
+  * @brief  Function implementing the defaultTask thread.
+  * @param  argument: Not used
+  * @retval None
+  */
+/* USER CODE END Header_StartDefaultTask */
+void StartDefaultTask(void *argument)
+{
+  /* USER CODE BEGIN StartDefaultTask */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END StartDefaultTask */
+}
+
+/* USER CODE BEGIN Header_StartTask03 */
+/**
+* @brief Function implementing the Tsk_ControlLed thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartTask03 */
+void StartTask03(void *argument)
+{
+  /* USER CODE BEGIN StartTask03 */
+	TickType_t last_wakeup_time = xTaskGetTickCount();
+	const TickType_t frequency = pdMS_TO_TICKS(20);
+  static Led_Control_st Temp_Led_Control_st ;
+  static GPIO_TypeDef* const Port_Led[3] = { LED_1_GPIO_Port, LED_2_GPIO_Port, LED_3_GPIO_Port};
+  static const uint16_t Pin_Led[3] = {LED_1_Pin, LED_2_Pin, LED_3_Pin};
+  
+  for(;;)
+  {
+    // Receive data select led
+    // Receive data toggle led
+    if( (xQueueReceive(Trigger_LEDHandle, &Temp_Led_Control_st, portMAX_DELAY) == pdPASS) )
+    {
+  
+        HAL_GPIO_WritePin(Port_Led[Temp_Led_Control_st.Led_selected_u8], Pin_Led[Temp_Led_Control_st.Led_selected_u8], (GPIO_PinState)Temp_Led_Control_st.led_state_u8 );
+    }
+    vTaskDelayUntil(&last_wakeup_time,frequency);
+    // osDelay(1);
+      //osDelay(500);
+    }
+  /* USER CODE END StartTask03 */
+}
+
+/* Private application code --------------------------------------------------*/
+/* USER CODE BEGIN Application */
+void HAL_GPIO_EXTI_Rising_Callback(uint16_t GPIO_Pin)
+{
+ 
+
+    switch (GPIO_Pin)
+    {
+      case BUTTON_SEL_LED_Pin:
+        Led_Control_t.Led_selected_u8 ++;
+        if(Led_Control_t.Led_selected_u8 > 2)
+        {
+          Led_Control_t.Led_selected_u8 = 0;
+        }
+        break;
+        case BUTTON_TOGGLE_LED_Pin:
+          Led_Control_t.led_state_u8 ^= (uint8_t)0x01;
+          
+        break;
+      default:
+        break;
+    }
+xQueueSendFromISR(Trigger_LEDHandle, &Led_Control_t, pdFALSE);
+
+}
+/* USER CODE END Application */
+
---
+++ CM4/Core/Src/gpio.c
@@ -0,0 +1,115 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    gpio.c
+  * @brief   This file provides code for the configuration
+  *          of all used GPIO pins.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "gpio.h"
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/*----------------------------------------------------------------------------*/
+/* Configure GPIO                                                             */
+/*----------------------------------------------------------------------------*/
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+
+/** Configure pins
+     PC15-OSC32_OUT   ------> RCC_OSC32_OUT
+     PC14-OSC32_IN   ------> RCC_OSC32_IN
+     PH0-OSC_IN   ------> RCC_OSC_IN
+     PH1-OSC_OUT   ------> RCC_OSC_OUT
+     PA5   ------> ADCx_INP19
+     PA4   ------> ADCx_INP18
+     PA0   ------> PWR_WKUP1
+*/
+void MX_GPIO_Init(void)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOI_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOG_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LED_3_GPIO_Port, LED_3_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LED_2_GPIO_Port, LED_2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : LED_1_Pin */
+  GPIO_InitStruct.Pin = LED_1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_MEDIUM;
+  HAL_GPIO_Init(LED_1_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : LED_3_Pin */
+  GPIO_InitStruct.Pin = LED_3_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LED_3_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : BUTTON_SEL_LED_Pin */
+  GPIO_InitStruct.Pin = BUTTON_SEL_LED_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(BUTTON_SEL_LED_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : USB_PWR_CC2_Pin USB_PWR_CC1_Pin */
+  GPIO_InitStruct.Pin = USB_PWR_CC2_Pin|USB_PWR_CC1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : LED_2_Pin */
+  GPIO_InitStruct.Pin = LED_2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LED_2_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : BUTTON_TOGGLE_LED_Pin */
+  GPIO_InitStruct.Pin = BUTTON_TOGGLE_LED_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(BUTTON_TOGGLE_LED_GPIO_Port, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI8_IRQn, 3, 0);
+  HAL_NVIC_EnableIRQ(EXTI8_IRQn);
+
+  HAL_NVIC_SetPriority(EXTI14_IRQn, 3, 0);
+  HAL_NVIC_EnableIRQ(EXTI14_IRQn);
+
+}
+
+/* USER CODE BEGIN 2 */
+
+/* USER CODE END 2 */
